ab562918461407052961fe4049160726a0f91f44
SyntaxTechnologies <moazzam@syntaxtechs.com>
2024-11-06 20:32:35 -0500

    virsu commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,13 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        for (int i=0;i<100;i++){
-            System.out.println(i);
-        }
-        for (int i=0;i<100;i++){
-            System.out.println(i);
-        }
-
+        System.out.println("i am the virus");
     }
 }
