2466770c781763d605edb4852937f6d4d56639b2
BESToloch2 <befanalex2@gmail.com>
2024-11-24 21:59:55 -0600

    tc

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("i am the virus");
         System.out.println("afafasf");
+        System.out.println("TestClone");
     }
 
 }
