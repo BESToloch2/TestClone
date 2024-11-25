fba010a11ea2f84ef856295450674629b002facf
BESToloch2 <befanalex2@gmail.com>
2024-11-24 21:58:07 -0600

    mo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("i am the virus");
+        System.out.println("afafasf");
     }
+
 }
